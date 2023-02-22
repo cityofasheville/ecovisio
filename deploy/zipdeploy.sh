27922cbd54ab1a6c0f47cc8574054b93934830b4
Jon Wilson <jontwilson@gmail.com>
2023-02-22 12:30:43 -0500

    separating out modules as lambda layer

--- deploy/zipdeploy.sh
+++ deploy/zipdeploy.sh
@@ -1,10 +1,20 @@
 echo "Packaging function as zip..."
+rm -f layer.zip
 rm -f function.zip
+rm -rf layer
+mkdir layer
+mkdir layer/nodejs
+cd layer/nodejs
+cp ../../../package.json .
+cp ../../../package-lock.json .
+npm ci
 cd ..
-pushd node_modules
-zip -r9q ../deploy/function.zip .
-popd
-zip -gq ./deploy/function.zip ./*.js package.json
+zip -rq ../layer.zip .
+cd ..
+rm -rf layer
+
+cd ..
+zip -q ./deploy/function.zip ./*.js package.json
 cd deploy
 echo "send it"
 # terraform apply -auto-approve -var-file=ca.tfvars
